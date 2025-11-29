ecfd0280f9dd54cb5353f617738796b223707ec7
Hannan Hashmi <81183236+hannanhashmi@users.noreply.github.com>
2025-11-29 18:48:42 +0530

    index.js

--- index.js
+++ index.js
@@ -89,5 +89,5 @@ app.post("/send", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log("Backend running on port " + PORT));
